75c5f9bf66792af3c249046cf21f2f3684d764e2
Yixuan <vivianxuanz@gmail.com>
2020-02-05 21:46:00 -0500

    router

---
+++ e-commerce-site/src/App.js
@@ -0,0 +1,24 @@
+import React from "react"
+import Header from "./components/Header"
+import {Switch, Route} from "react-router-dom"
+import "./styles.css"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
+
+function App() {    
+    return (
+        <div>
+            <Header />
+            <Switch>
+                <Route exact path="/">
+                    <Photos />
+                </Route>
+                <Route path="/cart">
+                    <Cart />
+                </Route>
+            </Switch>
+        </div>
+    )
+}
+
+export default App
